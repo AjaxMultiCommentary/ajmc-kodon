0bc80936e34ee2615e78d15424d07f7859ae96fe
Charles Pletcher <c@plet.ch>
2024-05-19 17:39:29 -0400

    Move ReadingEnvironment to its own component

---
+++ src/lib/components/ReadingEnvironment.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+    import type { Comment, PassageConfig, TextContainer } from '$lib/types';
+
+    import { page } from '$app/stores';
+    import CitableTextContainer from '$lib/components/CitableTextContainer.svelte';
+    import CollapsibleComment from '$lib/components/CollapsibleComment.svelte';
+    import Navigation from '$lib/components/Navigation.svelte';
+    import { marked } from 'marked';
+    import { onMount, tick } from 'svelte';
+
+    export let comments;
+    export let currentPassage
+    export let metadata;
+    export let passages;
+    export let textContainers;
+    export let commentToHighlight;
+
+
+
+    onMount(() => {
+        if (commentToHighlight) {
+            highlightComments([commentToHighlight]);
+        }
+    });
+
+    function handleHighlightComments(e: CustomEvent) {
+        highlightComments(e.detail);
+    }
+
+    async function highlightComments(commentsToHighlight: string[]) {
+        let foundComment: Comment | undefined;
+
+        comments = comments.map((comment: Comment) => {
+            if (commentsToHighlight.includes(comment.citable_urn as string)) {
+                if (!foundComment) {
+                    foundComment = comment;
+                }
+
+                return {
+                    ...comment,
+                    isHighlighted: true
+                };
+            }
+
+            return {
+                ...comment,
+                isHighlighted: false
+            };
+        });
+
+        if (foundComment && foundComment.citable_urn) {
+            await tick();
+
+            setTimeout(() => {
+                // @ts-ignore
+                document.getElementById(foundComment.citable_urn)?.scrollIntoView({ behavior: 'smooth' });
+            }, 200);
+        }
+    }
+</script>
+<article class="mx-auto w-full p-8">
+    <div class="pb-8">
+        <h1 class="text-2xl font-bold">{@html marked(metadata.title)}</h1>
+
+        <p>{@html marked(metadata.description)}</p>
+    </div>
+    <div class="grid grid-cols-10 gap-x-8 gap-y-2 h-screen max-h-[64rem]">
+        <section class="col-span-2">
+            <Navigation {passages} currentPassageUrn={currentPassage.urn} />
+        </section>
+        <section class="col-span-5 overflow-y-scroll -mt-4">
+            {#each textContainers as textContainer}
+                <CitableTextContainer {textContainer} on:highlightComments={handleHighlightComments} />
+            {/each}
+        </section>
+        <section class="overflow-y-scroll col-span-3 max-h-screen">
+            {#each comments as comment}
+                <CollapsibleComment {comment} />
+            {/each}
+        </section>
+    </div>
+</article>
--- src/routes/passages/[urn]/+page.svelte
+++ src/routes/passages/[urn]/+page.svelte
@@ -2,99 +2,41 @@
 	import type { Comment, PassageConfig, TextContainer } from '$lib/types';
 
 	import { page } from '$app/stores';
-	import CitableTextContainer from '$lib/components/CitableTextContainer.svelte';
-	import CollapsibleComment from '$lib/components/CollapsibleComment.svelte';
-	import Navigation from '$lib/components/Navigation.svelte';
-	import { marked } from 'marked';
-	import { onMount, tick } from 'svelte';
+	import ReadingEnvironment from '$lib/components/ReadingEnvironment.svelte';
 
 	export let data;
 
 	const commentToHighlight = $page.url.searchParams.get('gloss');
 
-	$: comments = (data.comments as Comment[]).map((comment: Comment) => {
-		if (comment.citable_urn === commentToHighlight) {
-			return {
-				...comment,
-				isHighlighted: true
-			};
-		}
+    $: comments = (data.comments as Comment[]).map((comment: Comment) => {
+        if (comment.citable_urn === commentToHighlight) {
+            return {
+                ...comment,
+                isHighlighted: true
+            };
+        }
 
-		return comment;
-	});
-	$: currentPassage = data.currentPassage as unknown as PassageConfig;
-	$: metadata = data.metadata;
-	$: passages = data.passages as unknown as PassageConfig[];
-	$: textContainers = data.textContainers as TextContainer[];
+        return comment;
+    });
+    $: currentPassage = data.currentPassage as unknown as PassageConfig;
+    $: metadata = data.metadata;
+    $: passages = data.passages as unknown as PassageConfig[];
+    $: textContainers = data.textContainers as TextContainer[];
 
-	onMount(() => {
-		if (commentToHighlight) {
-			highlightComments([commentToHighlight]);
-		}
-	});
 
-	function handleHighlightComments(e: CustomEvent) {
-		highlightComments(e.detail);
-	}
-
-	async function highlightComments(commentsToHighlight: string[]) {
-		let foundComment: Comment | undefined;
-
-		comments = comments.map((comment: Comment) => {
-			if (commentsToHighlight.includes(comment.citable_urn as string)) {
-				if (!foundComment) {
-					foundComment = comment;
-				}
-
-				return {
-					...comment,
-					isHighlighted: true
-				};
-			}
-
-			return {
-				...comment,
-				isHighlighted: false
-			};
-		});
-
-		if (foundComment && foundComment.citable_urn) {
-			await tick();
-
-			setTimeout(() => {
-				// @ts-ignore
-				document.getElementById(foundComment.citable_urn)?.scrollIntoView({ behavior: 'smooth' });
-			}, 200);
-		}
-	}
-
-	function stripMarkdown(s: string): string {
-		return s.replace(/_|\*/gi, '');
-	}
+    function stripMarkdown(s: string): string {
+        return s.replace(/_|\*/gi, '');
+    }
 </script>
 
 <svelte:head>
 	<title>{stripMarkdown(metadata.title)}</title>
 </svelte:head>
-<article class="mx-auto w-full p-8">
-	<div class="pb-8">
-		<h1 class="text-2xl font-bold">{@html marked(metadata.title)}</h1>
-
-		<p>{@html marked(metadata.description)}</p>
-	</div>
-	<div class="grid grid-cols-10 gap-x-8 gap-y-2 h-screen max-h-[64rem]">
-		<section class="col-span-2">
-			<Navigation {passages} currentPassageUrn={currentPassage.urn} />
-		</section>
-		<section class="col-span-5 overflow-y-scroll -mt-4">
-			{#each textContainers as textContainer}
-				<CitableTextContainer {textContainer} on:highlightComments={handleHighlightComments} />
-			{/each}
-		</section>
-		<section class="overflow-y-scroll col-span-3 max-h-screen">
-			{#each comments as comment}
-				<CollapsibleComment {comment} />
-			{/each}
-		</section>
-	</div>
-</article>
+<ReadingEnvironment
+	{comments}
+	{commentToHighlight}
+	{currentPassage}
+	{metadata}
+	{passages}
+	{textContainers}
+/>
