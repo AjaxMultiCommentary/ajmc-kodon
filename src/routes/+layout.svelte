04b413e65d6a549bc910b53ce6d6d47c08fa4c64
Charles Pletcher <pletcher@users.noreply.github.com>
2024-06-13 21:46:26 -0400

    Try removing the header to isolate the error

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
 	import '../app.css';
 
-	import { Header } from 'kodon';
-
 	import type { LayoutData } from './$types';
 
-	export let data: LayoutData;
+	// export let data: LayoutData;
 </script>
 
 <main class="px-4">
-	<Header title={data.title} staticPages={data.staticPages} />
 	<slot />
 </main>
