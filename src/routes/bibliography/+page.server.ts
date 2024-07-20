5fc34f57698410e5dab717c3fbc4aed81d467c40
Charles Pletcher <pletcher@users.noreply.github.com>
2024-07-20 11:40:10 -0400

    Add Wikidata bibliography support

--- src/routes/bibliography/+page.server.ts
+++ src/routes/bibliography/+page.server.ts
@@ -1,9 +1,32 @@
-import { loadBibliographies } from "kodon";
+import type { PageServerLoad } from './$types';
+
+import { loadBibliographies } from 'kodon';
 
 export const prerender = true;
 
-export async function load({ parent }) {
-    const parentData = await parent();
+export const load: PageServerLoad = async ({ parent }) => {
+	const parentData = await parent();
 
-    return loadBibliographies(parentData.config.bibliographies_directory);
-}
+	const { bibliographies, csls } = loadBibliographies(parentData.config.bibliographies_directory);
+	const data = (await import('$lib/assets/wikidata_citations.json')).default;
+
+	const wikidataCitations = data.map((citation: any) => {
+		const citedBy = citation.citedBy.map((citedBy: any) => {
+			return {
+				id: citedBy.cited.value.split('/').at(-1),
+				author: citedBy.authorLabel.value,
+				place: citedBy.placeLabel?.value,
+				pubdate: citedBy.pubdate.value,
+				publisher: citedBy.publisherLabel?.value,
+				title: citedBy.title.value
+			};
+		});
+
+		return {
+			...citation,
+			citedBy
+		};
+	});
+
+	return { bibliographies, csls, wikidataCitations };
+};
--- src/routes/bibliography/+page.svelte
+++ src/routes/bibliography/+page.svelte
@@ -1,10 +1,36 @@
 <script lang="ts">
-	import { Bibliography } from 'kodon';
+	import { Bibliography, WikidataBibliography } from 'kodon';
 
 	export let data;
 
 	$: bibliographies = data.bibliographies;
 	$: csls = data.csls;
+	$: wikidataCitations = data.wikidataCitations;
+	$: activeTab = 'core-bibliography';
 </script>
 
-<Bibliography {bibliographies} {csls} />
+<div role="tablist" class="tabs tabs-bordered">
+	<input
+		type="radio"
+		name="bibliography"
+		role="tab"
+		class="tab"
+		aria-label="Core Bibliography"
+		checked={activeTab === 'core-bibliography'}
+	/>
+	<div role="tabpanel" class="tab-content">
+		<Bibliography {bibliographies} {csls} />
+	</div>
+
+	<input
+		type="radio"
+		name="bibliography"
+		role="tab"
+		class="tab"
+		aria-label="Extended Bibliography"
+		checked={activeTab === 'extended-bibliography'}
+	/>
+	<div role="tabpanel" class="tab-content">
+		<WikidataBibliography citations={wikidataCitations} />
+	</div>
+</div>
